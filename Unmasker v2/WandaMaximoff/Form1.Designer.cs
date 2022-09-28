0460beece4440de91f787c44c1088d7dfc49e9e5
Your Name <you@example.com>
2022-09-28 19:06:00 +0800

    latest v2 and build

---
+++ Unmasker v2/WandaMaximoff/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace WandaMaximoff
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.StringValue = new System.Windows.Forms.TextBox();
+            this.btnUnmask = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.PasswordTxtbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PasswordResult = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StringValue
+            // 
+            this.StringValue.Location = new System.Drawing.Point(43, 40);
+            this.StringValue.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.StringValue.Name = "StringValue";
+            this.StringValue.Size = new System.Drawing.Size(495, 23);
+            this.StringValue.TabIndex = 0;
+            // 
+            // btnUnmask
+            // 
+            this.btnUnmask.Location = new System.Drawing.Point(43, 66);
+            this.btnUnmask.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnUnmask.Name = "btnUnmask";
+            this.btnUnmask.Size = new System.Drawing.Size(82, 22);
+            this.btnUnmask.TabIndex = 2;
+            this.btnUnmask.Text = "Unmask";
+            this.btnUnmask.UseVisualStyleBackColor = true;
+            this.btnUnmask.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 114);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(492, 74);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 66);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 22);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Mask";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // PasswordTxtbox
+            // 
+            this.PasswordTxtbox.Location = new System.Drawing.Point(43, 243);
+            this.PasswordTxtbox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.PasswordTxtbox.Name = "PasswordTxtbox";
+            this.PasswordTxtbox.Size = new System.Drawing.Size(492, 23);
+            this.PasswordTxtbox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(43, 220);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Hash Password";
+            // 
+            // PasswordResult
+            // 
+            this.PasswordResult.Location = new System.Drawing.Point(43, 316);
+            this.PasswordResult.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.PasswordResult.Multiline = true;
+            this.PasswordResult.Name = "PasswordResult";
+            this.PasswordResult.Size = new System.Drawing.Size(492, 64);
+            this.PasswordResult.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Location = new System.Drawing.Point(43, 299);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Result";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label3.Location = new System.Drawing.Point(43, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Result";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label4.Location = new System.Drawing.Point(43, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Input string here";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(43, 268);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 22);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Hash";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(280, 406);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 22);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(139, 267);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(82, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Unhash";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(632, 437);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PasswordResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PasswordTxtbox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnUnmask);
+            this.Controls.Add(this.StringValue);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " JSI Development Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox StringValue;
+        private System.Windows.Forms.Button btnUnmask;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox PasswordTxtbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox PasswordResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Unmasker v2/WandaMaximoff/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WandaMaximoff
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(StringValue.Text))
+            {
+                textBox1.Text = String.Empty;
+                textBox1.Text = RemoveStringMask(StringValue.Text);
+            } 
+            else
+                MessageBox.Show("Please provide string to proceed", "Can't do that");
+        }
+
+        public static string RemoveStringMask(string pMaskedString)
+        {
+            try
+            {
+                if (pMaskedString != null)
+                {
+                    int ctr = 0, ctrLen = 1;
+                    string SearchStr = null, UnmaskValue = null;
+                    string[] StringToInt = new string[28] { "V", "O", "N", "E", "A", "T", "U", "K", "Z", "Ñ", "F", "B", "M", "L", "S", "C", "I", "R", "J", "H", "W", "P", "Q", "X", "D", "Y", "G", " " };
+                    while (ctrLen <= pMaskedString.Length)
+                    {
+                        SearchStr = pMaskedString.Substring(ctrLen - 1, 3); ctr = 0;
+                        while (ctr < 28)
+                        {
+                            if (int.TryParse(SearchStr.Trim(), out int OutNum))
+                            {
+                                int.TryParse(SearchStr.Substring(1, 2).Trim(), out int Indx);
+                                if (SearchStr.Substring(0, 1) == "1")
+                                    UnmaskValue = UnmaskValue + StringToInt[Indx];
+                                else if (SearchStr.Substring(0, 1) == "0")
+                                    UnmaskValue = UnmaskValue + StringToInt[Indx].ToLower();
+                                ctr = 100;
+                            }
+                            else if (SearchStr.Substring(0, 1) == StringToInt[ctr])
+                            {
+                                UnmaskValue = UnmaskValue + ctr; ctr = 100;
+                            }
+                            ctr++;
+                        }
+                        if (ctr == 28) UnmaskValue = UnmaskValue + SearchStr.Substring(2, 1);
+                        ctrLen = ctrLen + 3;
+                    }
+                    return UnmaskValue;
+                }
+                else return null;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Can't do that");
+                return null;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(StringValue.Text))
+            {
+                textBox1.Text = String.Empty;
+                textBox1.Text = generateStringMask(StringValue.Text);
+            }
+            else
+                MessageBox.Show("Please provide string to proceed", "Can't do that");
+
+        }
+
+        public static string generateStringMask(string pStringToMask)
+        {
+            if (pStringToMask != null)
+            {
+                int ctr = 0, ctrLen = 1;
+                string SearchStr = null, MaskValue = null;
+                string[] StringToInt = new string[28] { "V", "O", "N", "E", "A", "T", "U", "K", "Z", "Ñ", "F", "B", "M", "L", "S", "C", "I", "R", "J", "H", "W", "P", "Q", "X", "D", "Y", "G", " " };
+                while (ctrLen <= pStringToMask.Length)
+                {
+                    SearchStr = pStringToMask.Substring(ctrLen - 1, 1); ctr = 0;
+                    while (ctr < 28)
+                    {
+                        string CheckThis = StringToInt[ctr];
+                        if (int.TryParse(SearchStr.Trim(), out int OutNum))
+                        {
+                            if (OutNum >= 27) OutNum = OutNum - 27;
+                            MaskValue = MaskValue + StringToInt[OutNum] + StringToInt[OutNum] + StringToInt[OutNum];
+                            ctr = 100;
+                        }
+                        else if (SearchStr.ToUpper() == StringToInt[ctr])
+                        {
+                            string SubMask = null;
+                            if (SearchStr == StringToInt[ctr])
+                            {
+                                if (ctr < 10) SubMask = "10" + ctr;
+                                else SubMask = "1" + ctr;
+                                MaskValue = MaskValue + SubMask;
+                            }
+                            else if (SearchStr != StringToInt[ctr])
+                            {
+                                if (ctr < 10) SubMask = "00" + ctr;
+                                else SubMask = "0" + ctr;
+                                MaskValue = MaskValue + SubMask;
+                            }
+                            ctr = 100;
+                        }
+                        ctr++;
+                    }
+                    if (ctr == 28)
+                        MaskValue = MaskValue + "//" + SearchStr;
+                    ctrLen++;
+                }
+                return MaskValue;
+            }
+            else return null;
+        }
+        string Password = "j1n15y5admin";
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(PasswordTxtbox.Text))
+            {
+                //MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+                /*byte[] _bytesToHash = Encoding.Unicode.GetBytes(generateStringMask(PasswordTxtbox.Text));
+                string _mPassword = Convert.ToBase64String(md5.ComputeHash(_bytesToHash));
+                PasswordResult.Text = String.Empty;
+                PasswordResult.Text = _mPassword;*/
+                PasswordResult.Text = Hasher.Encrypt(PasswordTxtbox.Text, Password);
+
+            }
+            else 
+                MessageBox.Show("Can't hash when empty", "Please input password");
+            
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            PasswordResult.Text = string.Empty;
+            PasswordTxtbox.Text = string.Empty;
+            textBox1.Text = string.Empty;
+            StringValue.Text = string.Empty;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(PasswordTxtbox.Text))
+            {
+                /*MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+                byte[] _bytesToHash = Encoding.Unicode.GetBytes(RemoveStringMask(PasswordTxtbox.Text));
+                string _mPassword = Convert.ToBase64String(_bytesToHash);
+                PasswordResult.Text = String.Empty;
+                PasswordResult.Text = _mPassword;*/
+                PasswordResult.Text = Hasher.Decrypt(PasswordTxtbox.Text, Password);
+            }
+            else
+                MessageBox.Show("Can't hash when empty", "Please input password");
+        }
+    }
+}
