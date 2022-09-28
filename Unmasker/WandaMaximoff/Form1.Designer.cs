3773ff41c5a06032a21cd1c5764fe6de343c7607
Your Name <you@example.com>
2022-09-28 19:00:44 +0800

    final

---
+++ Unmasker/WandaMaximoff/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace WandaMaximoff
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StringValue = new System.Windows.Forms.TextBox();
+            this.btnUnmask = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StringValue
+            // 
+            this.StringValue.Location = new System.Drawing.Point(51, 51);
+            this.StringValue.Name = "StringValue";
+            this.StringValue.Size = new System.Drawing.Size(565, 27);
+            this.StringValue.TabIndex = 0;
+            // 
+            // btnUnmask
+            // 
+            this.btnUnmask.Location = new System.Drawing.Point(51, 85);
+            this.btnUnmask.Name = "btnUnmask";
+            this.btnUnmask.Size = new System.Drawing.Size(94, 29);
+            this.btnUnmask.TabIndex = 2;
+            this.btnUnmask.Text = "Unmask";
+            this.btnUnmask.UseVisualStyleBackColor = true;
+            this.btnUnmask.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(51, 150);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(562, 97);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Mask";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 399);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnUnmask);
+            this.Controls.Add(this.StringValue);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "The Mask (1994)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox StringValue;
+        private System.Windows.Forms.Button btnUnmask;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Unmasker/WandaMaximoff/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WandaMaximoff
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = String.Empty;
+            textBox1.Text = RemoveStringMask(StringValue.Text);
+        }
+
+        public static string RemoveStringMask(string pMaskedString)
+        {
+            if (pMaskedString != null)
+            {
+                int ctr = 0, ctrLen = 1;
+                string SearchStr = null, UnmaskValue = null;
+                string[] StringToInt = new string[28] { "V", "O", "N", "E", "A", "T", "U", "K", "Z", "Ñ", "F", "B", "M", "L", "S", "C", "I", "R", "J", "H", "W", "P", "Q", "X", "D", "Y", "G", " " };
+                while (ctrLen <= pMaskedString.Length)
+                {
+                    SearchStr = pMaskedString.Substring(ctrLen - 1, 3); ctr = 0;
+                    while (ctr < 28)
+                    {
+                        if (int.TryParse(SearchStr.Trim(), out int OutNum))
+                        {
+                            int.TryParse(SearchStr.Substring(1, 2).Trim(), out int Indx);
+                            if (SearchStr.Substring(0, 1) == "1")
+                                UnmaskValue = UnmaskValue + StringToInt[Indx];
+                            else if (SearchStr.Substring(0, 1) == "0")
+                                UnmaskValue = UnmaskValue + StringToInt[Indx].ToLower();
+                            ctr = 100;
+                        }
+                        else if (SearchStr.Substring(0, 1) == StringToInt[ctr])
+                        {
+                            UnmaskValue = UnmaskValue + ctr; ctr = 100;
+                        }
+                        ctr++;
+                    }
+                    if (ctr == 28) UnmaskValue = UnmaskValue + SearchStr.Substring(2, 1);
+                    ctrLen = ctrLen + 3;
+                }
+                return UnmaskValue;
+            }
+            else return null;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text = String.Empty;
+            textBox1.Text = generateStringMask(StringValue.Text);
+        }
+
+        public static string generateStringMask(string pStringToMask)
+        {
+            if (pStringToMask != null)
+            {
+                int ctr = 0, ctrLen = 1;
+                string SearchStr = null, MaskValue = null;
+                string[] StringToInt = new string[28] { "V", "O", "N", "E", "A", "T", "U", "K", "Z", "Ñ", "F", "B", "M", "L", "S", "C", "I", "R", "J", "H", "W", "P", "Q", "X", "D", "Y", "G", " " };
+                while (ctrLen <= pStringToMask.Length)
+                {
+                    SearchStr = pStringToMask.Substring(ctrLen - 1, 1); ctr = 0;
+                    while (ctr < 28)
+                    {
+                        string CheckThis = StringToInt[ctr];
+                        if (int.TryParse(SearchStr.Trim(), out int OutNum))
+                        {
+                            if (OutNum >= 27) OutNum = OutNum - 27;
+                            MaskValue = MaskValue + StringToInt[OutNum] + StringToInt[OutNum] + StringToInt[OutNum];
+                            ctr = 100;
+                        }
+                        else if (SearchStr.ToUpper() == StringToInt[ctr])
+                        {
+                            string SubMask = null;
+                            if (SearchStr == StringToInt[ctr])
+                            {
+                                if (ctr < 10) SubMask = "10" + ctr;
+                                else SubMask = "1" + ctr;
+                                MaskValue = MaskValue + SubMask;
+                            }
+                            else if (SearchStr != StringToInt[ctr])
+                            {
+                                if (ctr < 10) SubMask = "00" + ctr;
+                                else SubMask = "0" + ctr;
+                                MaskValue = MaskValue + SubMask;
+                            }
+                            ctr = 100;
+                        }
+                        ctr++;
+                    }
+                    if (ctr == 28)
+                        MaskValue = MaskValue + "//" + SearchStr;
+                    ctrLen++;
+                }
+                return MaskValue;
+            }
+            else return null;
+        }
+    }
+}
